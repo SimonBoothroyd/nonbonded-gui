026cae74e69212ab125c0a2483c50de539088461
SimonBoothroyd <simon.boothroyd@colorado.edu>
2020-05-11 21:36:11 -0600

    Remove unused graph data.

--- src/app/features/projects/pages/training-results/training-results.component.ts
+++ src/app/features/projects/pages/training-results/training-results.component.ts
@@ -17,20 +17,6 @@ import { getOptimizationResultState } from '@core/store/results/results.selector
 export class TrainingResultsComponent implements OnInit {
   optimizationResults$: Observable<OptimizationResultsState>;
 
-  public graphData = {
-    data: [
-      {
-        x: [1, 2, 3],
-        y: [2, 6, 3],
-        type: 'scatter',
-        mode: 'lines+points',
-        marker: { color: 'red' },
-      },
-      { x: [1, 2, 3], y: [2, 5, 3], type: 'bar' },
-    ],
-    layout: { title: 'A Fancy Plot' },
-  };
-
   constructor(private store: Store<State>) {}
 
   ngOnInit(): void {
